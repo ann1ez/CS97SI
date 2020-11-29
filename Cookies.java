c3246bac7bce62c4c6e468258b39b2d96e309861
Annie Zhu <44847825+ann1ez@users.noreply.github.com>
2020-11-29 17:02:01 -0600

    Update Cookies.java

--- Cookies.java
+++ Cookies.java
@@ -3,13 +3,10 @@ package Mathematics;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-//import java.util.*;
 
 public class Cookies {
 
 	public static void main(String[] args) throws IOException {
-//		Scanner in = new Scanner(System.in);
-
 		float[][] points = new float[2][200];
 		int numpoints = 0;
 		
@@ -24,27 +21,6 @@ public class Cookies {
 			numpoints++;
             s = br.readLine();
         }
-        System.out.println("done");
-        
-//		boolean moreinput = true;
-//		while(moreinput) {
-//			try {
-//				points[0][numpoints] = Float.parseFloat(in.next()); //x-value
-//				points[1][numpoints] = Float.parseFloat(in.next()); //y-value
-//				numpoints++;
-//			}
-//			catch(InputMismatchException e){
-//				
-//			}
-//		}
-		
-		
-//		while(in.hasNext()) {
-//			points[0][numpoints] = Float.parseFloat(in.next()); //x-value
-//			points[1][numpoints] = Float.parseFloat(in.next()); //y-value
-//			numpoints++;
-//		}
-//		System.out.println("read inputs");
 		
 		int max = 0;
 		for(int i=0; i<numpoints; i++) {
@@ -52,9 +28,10 @@ public class Cookies {
 				float[][] centers = centers(points[0][i], points[1][i], points[0][j], points[1][j]);
 				max = Math.max(max, count(points, centers, numpoints, 0));
 				max = Math.max(max, count(points, centers, numpoints, 1));
+				//System.out.println(max);
 			}
 		}
-//		in.close();
+
 		System.out.println(max);
 		
 	}
@@ -65,17 +42,17 @@ public class Cookies {
 		
 		//magnitudes of AB and BC
 		float magAB = (float) Math.sqrt((x1-pointB[0])*(x1-pointB[0]) + (y1-pointB[1])*(y1-pointB[1]));
-		float magBC = (float) Math.sqrt(6.25*6.25 - magAB*magAB);
+		float magBC = (float) Math.sqrt(6.25 - magAB*magAB);
 		
 		//vectors of AB and BC
-		float[] vectorAB = {x1-pointB[0], y1-pointB[0]};
-		float[] vectorBC = {(x2 - x1)/magAB * magBC, (y2 - y1)/magAB * magBC};
+		float[] vectorAB = {pointB[0]-x1, pointB[1]-y1};
+		float[] vectorBC = {(pointB[1]-y1)/magAB * magBC, (pointB[0]-x1)/magAB * magBC};
 		
 		float[][] centers = new float[2][2];
-		centers[0][0] = vectorAB[0] + vectorBC[0] + x1; // x 
-		centers[0][1] = vectorAB[1] + vectorBC[1] + x1; // y
-		centers[0][0] = vectorAB[0] - vectorBC[0] + x1; // x
-		centers[0][1] = vectorAB[1] - vectorBC[1] + x1; // y
+		centers[0][0] = x1 + vectorAB[0] + vectorBC[0]; // x 
+		centers[0][1] = y1 + vectorAB[1] + vectorBC[1]; // y
+		centers[1][0] = x2 - vectorAB[0] - vectorBC[0]; // x
+		centers[1][1] = y2 - vectorAB[1] - vectorBC[1]; // y
 		
 		return centers;
 	}
@@ -83,7 +60,8 @@ public class Cookies {
 	public static int count(float[][] points, float[][] centers, int numpoints, int n) {
 		int count =0;
 		for(int k=0; k<numpoints; k++) {
-			if(Math.pow(points[0][k]-centers[n][0],2) +  Math.pow(points[1][k]-centers[n][1],2) >= 6.25) {
+			double val = Math.pow(points[0][k]-centers[n][0],2) +  Math.pow(points[1][k]-centers[n][1],2);
+			if(val <= 6.25) {
 				count++;
 			}
 		}
